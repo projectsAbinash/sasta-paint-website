3019b58b95ffc4ed5142b742d314f03280833592
Abinash Karmakar <codeAbinash@gmail.com>
2022-12-24 15:47:15 +0530

    UI changes as required

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -74,7 +74,7 @@ const Home = () => {
                     <div className="left">
 
                         <img src={images.pp} />
-                        <h2 className="helloName">Hello👋🏻{profileData?.name ? (', ' + profileData.name) : ''}</h2>
+                        <h2 className="helloName">Hello{profileData?.name ? (', ' + profileData.name) : ''}👋🏻</h2>
                     </div>
                     <div className="right">
                         <div className="countDiv">
--- src/pages/NewOrder.tsx
+++ src/pages/NewOrder.tsx
@@ -44,87 +44,91 @@ function NewOrder() {
             <div className="container">
                 {/* <form action=""> */}
 
-                    <div className="fileDiv">
-                        <div className="left">
-                            <img src={icons.file_pdf_solid} />
-                            <span className="fileName noSelect">Attach a PDF file</span>
-                        </div>
-
-                        <div className="right">
-                            <img src={icons.plus_solid} />
-                        </div>
+                <div className="fileDiv">
+                    <div className="left">
+                        <img src={icons.file_pdf_solid} />
+                        <span className="fileName noSelect">Attach a PDF file</span>
                     </div>
 
+                    <div className="right">
+                        <img src={icons.plus_solid} />
+                    </div>
+                </div>
 
 
-                    <div className="details">
-                        <p className="label">Details</p>
-                        <input type="text" className='inp title' placeholder='Title of your document' />
-                        <textarea name="Instructions" id="instructions" rows={2}
-                            placeholder='Enter any instructions that should be carried out while printing your file.'
-                        ></textarea>
-                        <p className="label">Number of Copies</p>
-                        <div className="copies">
-                            <input
-                                type="number" className='inp' name="noOfCopies" max={1000}
-                                id="noOfCopies" placeholder='Number of copies' min={1}
-                                value={noOfCopies} onInput={(e: any) => { updateCopyByInput(e) }}
-                            />
-                            <button className='btn-' onClick={() => { changeNoOfCopies(-1) }}><img src={icons.minus_solid} /></button>
-                            <button className='btn+' onClick={() => { changeNoOfCopies(1) }}><img src={icons.plus_solid} /></button>
+
+                <div className="details">
+                    <p className="label">Details</p>
+                    <input type="text" className='inp title' placeholder='Title of your document' />
+                    <textarea name="Instructions" id="instructions" rows={2}
+                        placeholder='Enter any instructions that should be carried out while printing your file.'
+                    ></textarea>
+                    <p className="label">Number of Copies</p>
+                    <div className="copies">
+                        <input
+                            type="number" className='inp' name="noOfCopies" max={1000}
+                            id="noOfCopies" placeholder='Number of copies' min={1}
+                            value={noOfCopies} onInput={(e: any) => { updateCopyByInput(e) }}
+                        />
+                        <button className='btn-' onClick={() => { changeNoOfCopies(-1) }}><img src={icons.minus_solid} /></button>
+                        <button className='btn+' onClick={() => { changeNoOfCopies(1) }}><img src={icons.plus_solid} /></button>
+                    </div>
+                </div>
+
+
+                <div className="config">
+                    <p className="label">Print Configurations</p>
+                    <div className="selectColor">
+                        <div className="select" onClick={() => { colorRadio1.current.click() }}>
+                            <input ref={colorRadio1} type="radio" name="printColor" id="color1" value='b&w' checked onChange={() => { }} />
+                            <span>Black & White</span>
+                        </div>
+                        <div className="select" onClick={() => {
+                            // colorRadio2.current.click()
+                            updateAlertBoxDetails({
+                                active: true,
+                                title: 'Alert',
+                                content: 'Feature is coming Soon',
+                                buttonText: 'OK'
+                            })
+                        }}>
+                            <input ref={colorRadio2} type="radio" name="printColor" id="color1" value='color' disabled />
+                            <span>Color</span>
                         </div>
                     </div>
+                </div>
 
-
-                    <div className="config">
-                        <p className="label">Print Configurations</p>
-                        <div className="selectColor">
-                            <div className="select" onClick={() => { colorRadio1.current.click() }}>
-                                <input ref={colorRadio1} type="radio" name="printColor" id="color1" value='b&w' checked onChange={()=>{}}/>
-                                <span>Black & White</span>
-                            </div>
-                            <div className="select" onClick={() => {
-                                // colorRadio2.current.click()
-                                updateAlertBoxDetails({
-                                    active: true,
-                                    title: 'Alert',
-                                    content: 'Feature is coming Soon',
-                                    buttonText: 'OK'
-                                })
-                            }}>
-                                <input ref={colorRadio2} type="radio" name="printColor" id="color1" value='color' disabled />
-                                <span>Color</span>
-                            </div>
-                        </div>
-                    </div>
-
+                <div className="selectPage">
+                    <p className="label">Page Sides</p>
                     <div className="selectPage">
-                        <p className="label">Page Sides</p>
-                        <div className="selectPage">
-                            <div className="select" onClick={() => { pageSide2.current.click() }}>
-                                <input ref={pageSide2} type="radio" name="pageSide" id="color1" value='2' checked onChange={()=>{}}/>
-                                <div className="text">
-                                    <span >Two-Sided</span>
-                                    <div className="price">
-                                        <span className='price cancel'>$1.00</span>
-                                        <span className='price'>$0.55/page</span>
-                                    </div>
+                        <div className="select" onClick={() => { pageSide2.current.click() }}>
+                            <input ref={pageSide2} type="radio" name="pageSide" id="color1" value='2' checked onChange={() => { }} />
+                            <div className="text">
+                                <span >Two-Sided</span>
+                                <div className="price">
+                                    <span className='price cancel'>$1.00</span>
+                                    <span className='price'>$0.55/page</span>
                                 </div>
                             </div>
-                            <div className="select" onClick={() => { pageSide1.current.click() }}>
-                                <input ref={pageSide1} type="radio" name="pageSide" id="color1" value='1'/>
-                                <div className="text">
-                                    <span >One-Sided</span>
-                                    <div className="price">
-                                        <span className='price cancel'>$1.5.00</span>
-                                        <span className='price'>$0.65/page</span>
-                                    </div>
+                        </div>
+                        <div className="select" onClick={() => { pageSide1.current.click() }}>
+                            <input ref={pageSide1} type="radio" name="pageSide" id="color1" value='1' />
+                            <div className="text">
+                                <span >One-Sided</span>
+                                <div className="price">
+                                    <span className='price cancel'>$1.5.00</span>
+                                    <span className='price'>$0.65/page</span>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <br />
+                </div>
+                <br />
+                <div className="buttons">
+                    <input type="submit" name="" id="" value={'Add New'} className='btnLarge' onClick={handelSubmit} />
                     <input type="submit" name="" id="" value={'Next'} className='btnLarge' onClick={handelSubmit} />
+
+                </div>
 
                 {/* </form> */}
             </div>
